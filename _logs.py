3b95ed958b19bf1fd3dd07536a81dd170aa49c3b
Palma.luv <84909252+Palma-IT-DUO@users.noreply.github.com>
2023-01-08 00:46:26 +0200

    v1.0.0

---
+++ _logs.py
@@ -0,0 +1,63 @@
+import six 
+
+from PyInquirer import ValidationError, Validator, prompt
+from prompt_toolkit import document as doc
+from main import config
+
+try:
+    import colorama
+    colorama.init()
+except ImportError:
+    colorama = None
+
+try:
+    from termcolor import colored
+except ImportError:
+    colored = None    
+
+array_logo = ["    ______                   ______ _____    ___                      ",
+              "   / __/ /____ ___ ___ _    / ___(_) _/ /_  / _ \___ ________ ___ ____",
+              "  _\ \/ __/ -_) _ `/  ' \  / (_ / / _/ __/ / ___/ _ `/ __(_-</ -_) __/",
+              " /___/\__/\__/\_,_/_/_/_/  \___/_/_/ \__/ /_/   \_,_/_/ /___/\__/_/   " ]
+
+# text output
+def log(str,color="white"):
+    if colored: 
+        six.print_(colored(str, color))
+    else: 
+        six.print_(str)
+
+class PointValidator(Validator):
+    def validate(self, document: doc.Document):
+        value = document.text
+        try:
+            value = int(value)
+        except Exception:
+            raise ValidationError(message = 'Value should be greater than 0', cursor_position = len(document.text))
+
+        if value <= 0:
+            raise ValidationError(message = 'Value should be greater than 0', cursor_position = len(document.text))
+        return True
+
+def ask(type, name, msg, validate=None, choices=[]):
+    questions = [
+        {
+            'type'      : type, 
+            'name'      : name,
+            'message'   : msg,
+            'validate'  : validate
+        }
+    ]
+    if choices:
+        questions[0].update({'choices':choices})
+    answers = prompt(questions)
+    return answers
+
+def askCookie():
+        cookie = ask('input', 'cookie',
+                    'Enter PHPSESSID cookie'
+        )
+        config['DEFAULT']['cookie'] = cookie['cookie']
+        with open('config.ini', 'w') as cofFILE :
+            config.write(cofFILE)
+        return cookie['cookie']
---
+++ main.py
@@ -0,0 +1,51 @@
+#    ______                   ______ _____    ___                      
+#   / __/ /____ ___ ___ _    / ___(_) _/ /_  / _ \___ ________ ___ ____
+#  _\ \/ __/ -_) _ `/  ' \  / (_ / / _/ __/ / ___/ _ `/ __(_-</ -_) __/
+# /___/\__/\__/\_,_/_/_/_/  \___/_/_/ \__/ /_/   \_,_/_/ /___/\__/_/   
+#                                                                    
+#
+
+# from method.method import SteamGift
+import configparser
+import _logs as _l
+
+# from PyInstaller import (Token, Error, JsonPrint, prompt)
+config = configparser.ConfigParser()
+
+def run(): 
+    from method.method import SteamGift as steamGif
+    for i in range(4): 
+        _l.log(_l.array_logo[i], "green")
+    _l.log("\nEnjoy using our product!","white")
+    _l.log("Created by: github.com/PalmaLuv\nStay tuned for further app updates","red")
+
+    config.read('config.ini')
+    if not config['DEFAULT'].get('cookie'):
+        cookie = _l.askCookie()
+    else: 
+        InputCookie = _l.ask('confirm', 'reenter',
+        'Do you want to enter new cookie?')['reenter']
+        if InputCookie:
+            cookie = _l.askCookie()
+        else:
+            cookie = config['DEFAULT'].get('cookie')
+    pinnedGames = _l.ask('confirm', 'pinned', 
+    'Should the bot enter pinned games?')['pinned']
+    
+    giftTYPE = _l.ask('list', 'gift_type', 'Select type:',
+    choices=[
+        'All',
+        'Wishlist',
+        'Recommended',
+        'Copies',
+        'DLC',
+        'New'
+    ])['gift_type']
+    minPoin = _l.ask('input', 'min_points',
+    'What is the minimum number of points to remain?', _l.PointValidator)['min_points']
+    sg = steamGif(cookie, giftTYPE, pinnedGames, minPoin)
+    sg.start()
+
+
+if __name__ == '__main__':
+    run()
